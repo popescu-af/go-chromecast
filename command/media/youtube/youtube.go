5bb6985dc0439722eff6af593fd6b2a848115e28
oliverpool <oliverpool@hotmail.fr>
2018-06-02 20:36:07 +0200

    parse youtube URL

--- command/media/youtube/youtube.go
+++ command/media/youtube/youtube.go
@@ -1,6 +1,10 @@
 package youtube
 
 import (
+	"fmt"
+	"net/url"
+	"path"
+
 	"github.com/oliverpool/go-chromecast"
 	"github.com/oliverpool/go-chromecast/command/media"
 )
@@ -23,4 +27,37 @@ func (a App) Load(id string, options ...media.Option) (<-chan []byte, error) {
 		StreamType:  "BUFFERED",
 	}
 	return a.App.Load(item, options...)
+}
+
+func (a App) LoadURL(rawurl string, options ...media.Option) (<-chan []byte, error) {
+	id, err := ExtractID(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	return a.Load(id, options...)
+}
+
+func ExtractID(rawurl string) (string, error) {
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return "", fmt.Errorf("could not parse url '%s': %v", rawurl, err)
+	}
+
+	hosts := map[string]struct{}{
+		"youtube-nocookie.com":     struct{}{},
+		"www.youtube-nocookie.com": struct{}{},
+		"youtu.be":                 struct{}{},
+		"youtube.com":              struct{}{},
+		"www.youtube.com":          struct{}{},
+	}
+	if _, ok := hosts[u.Host]; !ok {
+		return "", fmt.Errorf("unsupported host: %s", u.Host)
+	}
+	if id := u.Query().Get("v"); id != "" {
+		return id, nil
+	}
+	if id := path.Base(u.Path); id != "" {
+		return id, nil
+	}
+	return "", fmt.Errorf("could not find id inside URL: %s", rawurl)
 }
---
+++ command/media/youtube/youtube_test.go
@@ -0,0 +1,30 @@
+package youtube_test
+
+import (
+	"testing"
+
+	"github.com/oliverpool/go-chromecast/command/media/youtube"
+)
+
+func TestURLParsing(t *testing.T) {
+	cc := []struct {
+		url      string
+		expected string
+	}{
+		{"https://www.youtube.com/watch?v=b-GIBLX3nAk", "b-GIBLX3nAk"},
+		{"https://youtu.be/b-GIBLX3nAk", "b-GIBLX3nAk"},
+		{"https://youtu.be/b-GIBLX3nAk?t=1s", "b-GIBLX3nAk"},
+		{"https://www.youtube.com/embed/b-GIBLX3nAk", "b-GIBLX3nAk"},
+		{"https://www.youtube-nocookie.com/embed/b-GIBLX3nAk?start=10", "b-GIBLX3nAk"},
+	}
+
+	for _, c := range cc {
+		got, err := youtube.ExtractID(c.url)
+		if got != c.expected {
+			t.Errorf("got '%s', expected '%s' for '%s'", got, c.expected, c.url)
+		}
+		if err != nil {
+			t.Errorf("got unexpected error: %v", err)
+		}
+	}
+}
