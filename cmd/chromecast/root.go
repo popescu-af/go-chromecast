6e8a79a318e662fa14b8bc362930be2579042f29
oliverpool <oliverpool@hotmail.fr>
2018-06-23 00:12:19 +0200

    prevent outputting error twice

--- cmd/chromecast/root.go
+++ cmd/chromecast/root.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"time"
 
@@ -37,7 +36,6 @@ func init() {
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
