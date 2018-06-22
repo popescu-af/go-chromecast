3eed46ce9e6d190bbe1550382fa807adf0a06bf2
oliverpool <oliverpool@hotmail.fr>
2018-06-22 22:54:35 +0200

    add basic list command

---
+++ cmd/chromecast/list.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	chromecast "github.com/oliverpool/go-chromecast"
+	"github.com/oliverpool/go-chromecast/discovery"
+	"github.com/oliverpool/go-chromecast/discovery/zeroconf"
+	"github.com/oliverpool/go-chromecast/log"
+	"github.com/spf13/cobra"
+)
+
+var timeout time.Duration
+
+func init() {
+	listCmd.Flags().DurationVarP(&timeout, "timeout", "t", 10*time.Second, "Duration before stopping looking for chromecast(s)")
+	rootCmd.AddCommand(listCmd)
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Print all the chromecast found in the network",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger := log.New(os.Stdout)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		scanner := zeroconf.Scanner{Logger: logger}
+		allDevices := make(chan *chromecast.Device, 5)
+		uniqDevices := make(chan *chromecast.Device, 5)
+
+		worker, err := scanner.Scan(ctx, allDevices)
+		if err != nil {
+			return fmt.Errorf("could not initialize scanner: %v", err)
+		}
+		go worker()
+		go discovery.Uniq(allDevices, uniqDevices)
+		for d := range uniqDevices {
+			fmt.Printf("- %s [Addr=%s; uuid=%s; type=%s; status=%s]\n",
+				d.Name(), d.Addr(), d.ID(), d.Type(), d.Status())
+		}
+		return nil
+	},
+}
