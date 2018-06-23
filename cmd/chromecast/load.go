240ceb6b5b96a0814e3b4feea17910991cc09764
oliverpool <oliverpool@hotmail.fr>
2018-06-23 18:35:02 +0200

    add load subcommand

---
+++ cmd/chromecast/load.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/oliverpool/go-chromecast"
+
+	"github.com/oliverpool/go-chromecast/command/media"
+	"github.com/oliverpool/go-chromecast/command/media/defaultreceiver"
+	"github.com/oliverpool/go-chromecast/command/media/defaultreceiver/tatort"
+	"github.com/oliverpool/go-chromecast/command/media/defaultreceiver/tvnow"
+	"github.com/oliverpool/go-chromecast/command/media/youtube"
+	"github.com/oliverpool/go-chromecast/command/urlreceiver"
+	"github.com/spf13/cobra"
+)
+
+var loadRequestTimeout time.Duration
+
+var useLoader string
+var loaders = []namedLoader{
+	{"tatort", tatort.URLLoader},
+	{"tvnow", tvnow.URLLoader},
+	{"youtube", youtube.URLLoader},
+	{"default", defaultreceiver.URLLoader},
+	{"urlreceiver", urlreceiver.URLLoader},
+}
+
+type namedLoader struct {
+	name   string
+	loader media.URLLoader
+}
+
+func (nl namedLoader) load(client chromecast.Client, status chromecast.Status, rawurl string) (<-chan []byte, error) {
+	loader, err := nl.loader(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	return loader(client, status)
+}
+
+func init() {
+	loadCmd.Flags().DurationVarP(&loadRequestTimeout, "request-timeout", "r", 10*time.Second, "Duration to wait for a reply to the load request")
+	var ll []string
+	for _, l := range loaders {
+		ll = append(ll, l.name)
+	}
+	loadCmd.Flags().StringVarP(&useLoader, "loader", "l", "", "Loader to use (supported loaders: "+strings.Join(ll, ", ")+")")
+	rootCmd.AddCommand(loadCmd)
+}
+
+var loadCmd = &cobra.Command{
+	Use:   "load [url]",
+	Short: "Load a URL",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		rawurl := args[0]
+
+		logger, ctx, cancel := flags()
+		defer cancel()
+
+		client, status, err := GetClientWithStatus(ctx, logger)
+		if err != nil {
+			return fmt.Errorf("could not get a client: %v", err)
+		}
+
+		for _, l := range loaders {
+			var c <-chan []byte
+			var err error
+
+			if useLoader != "" {
+				if l.name != useLoader {
+					continue
+				}
+				c, err = l.load(client, status, rawurl)
+				if err != nil {
+					return err
+				}
+			} else {
+				c, err = l.load(client, status, rawurl)
+				if err != nil {
+					logger.Log("loader", l.name, "state", "loading", "err", err)
+					continue
+				}
+				fmt.Printf("Loading with %s\n", l.name)
+			}
+			select {
+			case <-c:
+			case <-time.After(loadRequestTimeout):
+				logger.Log("loader", l.name, "err", "load request didn't return after 10s")
+			}
+			return nil
+		}
+		if useLoader != "" {
+			return fmt.Errorf("unknown loader '%s'", useLoader)
+		}
+		return fmt.Errorf("no supported loader found for %s", rawurl)
+	},
+}
